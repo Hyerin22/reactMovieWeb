0ba39fe324aa34400650c6d51a47c2e0752d79e8
Hyerin <hyerin.bcit@gmail.com>
2023-03-01 17:19:34 -0800

    making Movie component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  // api : https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
+  // movie api : https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
+  // movie details api: https://yts.mx/api/v2/movie_details.json?movie_id=15527
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  // 2. async & await methond
-  const getMovies = async () => {
-    /*     
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
-     */
 
+  const getMovies = async () => {
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
@@ -21,21 +16,12 @@ function App() {
     setMovies(json.data.movies);
     setLoading(false);
   };
+
   useEffect(() => {
-    /*
-    2. fetch & then method
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-      */
     getMovies();
   }, []);
   console.log(movies);
+
   return (
     <div>
       {loading ? (
@@ -43,18 +29,15 @@ function App() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                <li>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </li>
-              </ul>
-            </div>
+            <Movie
+              // key is only for react.js
+              // when you reder the component, key should be in the App.js
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
