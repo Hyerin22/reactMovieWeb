8a8688cff0c332c98c77d9f5390231d98401906b
Hyerin <hyerin.bcit@gmail.com>
2023-03-21 21:36:44 -0700

    pagination done

---
+++ src/components/Pagination.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./Pagination.module.css";
+
+export default function Pagination({
+  currentPage,
+  totalPages,
+  handlePageClick,
+  handlePrevPage,
+  handleNextPage,
+}) {
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li key={i} className={styles.eachNumber}>
+          <button onClick={() => handlePageClick(i)}>{i}</button>
+        </li>
+      );
+    }
+    return pageNumbers;
+  };
+  return (
+    <div className={styles.container}>
+      <button
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+        className={styles.arrowButton}
+      >
+        <FontAwesomeIcon icon={faChevronLeft} color="#66FCF1" size="2x" />
+      </button>
+      <ul className={styles.numbers}>{renderPageNumbers()}</ul>
+      <button
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+        className={styles.arrowButton}
+      >
+        <FontAwesomeIcon icon={faChevronRight} color="#66FCF1" size="2x" />
+      </button>
+    </div>
+  );
+}
--- src/pages/Romance.js
+++ src/pages/Romance.js
@@ -5,22 +5,47 @@ import styles from "./GenrePages.module.css";
 import Nav from "../components/Nav";
 import MovieInfo from "../components/MovieInfo";
 import Footer from "../components/Footer";
+import Pagination from "../components/Pagination";
 
 export default function Romance() {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const getTrending = async () => {
+  const moviesPerPage = 4;
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
+
+  const getData = async () => {
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?&genre=romance&sort_by=rating`
       )
     ).json();
     setMovies(json.data.movies);
+    setTotalPages(Math.ceil(json.data.movies.length / moviesPerPage));
   };
 
   useEffect(() => {
-    getTrending();
+    getData();
   }, []);
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <div>
@@ -28,7 +53,7 @@ export default function Romance() {
       <div>
         <p className={styles.title}># Romance</p>
         <div className={styles.movieContent}>
-          {movies.map((movie, index) => (
+          {currentMovies.map((movie) => (
             <div>
               {/* Movies */}
               <MovieInfo
@@ -46,6 +71,14 @@ export default function Romance() {
             </div>
           ))}
         </div>
+        <Pagination
+          lastPage={indexOfLastMovie}
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageClick={handlePageClick}
+          handlePrevPage={handlePrevPage}
+          handleNextPage={handleNextPage}
+        />
       </div>
       <Footer />
     </div>
