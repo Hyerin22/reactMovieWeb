08ca07575f7ee4bff144a670d6bf8c28043bed36
Hyerin <hyerin.bcit@gmail.com>
2023-04-09 01:37:11 -0700

    added pagination in favourite page

--- src/pages/Favourite.js
+++ src/pages/Favourite.js
@@ -3,19 +3,49 @@ import React, { useState, useEffect } from "react";
 import styles from "./Favourite.module.css";
 import Nav from "../components/Nav";
 import MovieInfo from "../components/MovieInfo";
+import Pagination from "../components/Pagination";
 import Footer from "../components/Footer";
 
 export default function Favourite() {
+  // for data
   const [favMovies, setFavMovies] = useState([]);
+
+  // for pagination
+  const [currentPage, setCurrentPage] = useState(1);
 
   // get favList
   useEffect(() => {
     const savedFavMovies = localStorage.getItem("favList");
     if (savedFavMovies) {
       setFavMovies(JSON.parse(savedFavMovies));
+      // setTotalPages(Math.ceil(favMovies.length) / moviesPerPage);
     }
     // console.log(savedFavMovies);
   }, []);
+
+  const moviesPerPage = 4;
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentPageMoveis = favMovies.slice(
+    indexOfFirstMovie,
+    indexOfLastMovie
+  );
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(favMovies.length) / moviesPerPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <div>
@@ -23,8 +53,8 @@ export default function Favourite() {
       <div>
         <p className={styles.title}>My List</p>
         <div className={styles.container}>
-          {favMovies.length > 0 ? (
-            favMovies.map((movie) => (
+          {currentPageMoveis.length > 0 ? (
+            currentPageMoveis.map((movie) => (
               <MovieInfo
                 key={movie.id}
                 id={movie.id}
@@ -42,6 +72,13 @@ export default function Favourite() {
             <p className={styles.noMovie}>No movies yet in your list.</p>
           )}
         </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(favMovies.length / moviesPerPage)}
+          handlePageClick={handlePageClick}
+          handleNextPage={handleNextPage}
+          handlePrevPage={handlePrevPage}
+        />
       </div>
       <Footer />
     </div>
